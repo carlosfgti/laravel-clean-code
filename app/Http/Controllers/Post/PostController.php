2c3608e2de11d7644aa21392768e873ae43d8779
Carlos Ferreira <conta@especializati.com.br>
2018-05-21 14:23:24 -0300

    Store Post and Validate

--- app/Http/Controllers/Post/PostController.php
+++ app/Http/Controllers/Post/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Http\Requests\Post\StorePostFormRequest;
 
 class PostController extends Controller
 {
@@ -19,5 +20,15 @@ class PostController extends Controller
     public function index()
     {
     	return view('posts.index');
+    }
+
+
+    public function store(StorePostFormRequest $request)
+    {
+    	$this->post->create($request->all());
+
+    	return redirect()
+    				->with('success', 'Sucesso ao Cadastrar!!!')
+    				->route('posts.index');
     }
 }
