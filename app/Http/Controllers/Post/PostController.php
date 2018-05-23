a497f60532d47ebe8c25466d29e6c46c44a3164c
Carlos Ferreira <conta@especializati.com.br>
2018-05-23 14:37:21 -0300

    Show Posts in View

--- app/Http/Controllers/Post/PostController.php
+++ app/Http/Controllers/Post/PostController.php
@@ -19,7 +19,9 @@ class PostController extends Controller
 
     public function index()
     {
-    	return view('posts.index');
+        $posts = $this->post->all();
+
+    	return view('posts.index', compact('posts'));
     }
 
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        Posts
+        <table class="table">
+        	<tr>
+        		<th width="50">ID</th>
+        		<th>Nome</th>
+        	</tr>
+        	@forelse ($posts as $post)
+	        	<tr>
+	        		<td>{{ $post->id }}</td>
+	        		<td>{{ $post->name }}</td>
+	        	</tr>
+        	@empty
+        	@endforelse
+        </table>
     </div>
 </div>
 @endsection
