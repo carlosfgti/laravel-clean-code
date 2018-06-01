6929f004b9482b58f65d300edbc8debc66cb5685
Carlos Ferreira <conta@especializati.com.br>
2018-06-01 17:34:53 -0300

    Set Charset Template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8">
 		<title>@yield('title')</title>
 
 		<!--Bootstrap-->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 	</head>
 	<body>
-		
+
 	</body>
 </html>
