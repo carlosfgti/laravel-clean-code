6535ad1cdf6d48e100bebaa468436290db9178c4
Carlos Ferreira <conta@especializati.com.br>
2018-06-01 17:34:20 -0300

    Add Bootstrap Template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>@yield('title')</title>
-</head>
-<body>
+	<head>
+		<title>@yield('title')</title>
 
-</body>
+		<!--Bootstrap-->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	</head>
+	<body>
+		
+	</body>
 </html>
