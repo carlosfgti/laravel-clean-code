b9ee13c6f603d3c830f046611fc5a5e35f64928c
Carlos Ferreira <conta@especializati.com.br>
2018-06-01 17:35:39 -0300

    Content Template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,8 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 	</head>
 	<body>
-
+		<div class="container">
+			@yield('content')
+		</div>
 	</body>
 </html>
