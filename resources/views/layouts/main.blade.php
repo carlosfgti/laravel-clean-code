6fabff7bd3659cc58300327936577908d6eba0b7
Carlos Ferreira <conta@especializati.com.br>
2018-06-06 18:35:40 -0300

    Add app.js template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,9 +8,14 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 	</head>
 	<body>
-		<div class="container">
-			@yield('content')
+		<div id="app">
+			<div class="container">
+				@yield('content')
+			</div>
 		</div>
+
+		<!--Laravel Mix APP-->
+		<script src="{{ url('js/app.js') }}"></script>
 
 		<!--dynamic scripts-->
 		@stacks('scripts')
